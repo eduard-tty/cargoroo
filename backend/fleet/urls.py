bc005623c3257934b369946205d9a628a67775ee
eduard <elohmann@xs4all.nl>
2023-05-01 23:47:51 +0200

    match variable names inurls to method signatures

--- backend/fleet/urls.py
+++ backend/fleet/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('fleet/', views.list_fleets),
-    path('fleet/<str:id>', views.fleet_id_dispatch),
-    path('fleet/<str:id>/bike', views.list_bikes_in_fleet),
-    path('bike/<str:id>', views.bike_id_dispatch),
+    path('fleet/<str:fid>', views.fleet_id_dispatch),
+    path('fleet/<str:fid>/bike', views.list_bikes_in_fleet),
+    path('bike/<str:bid>', views.bike_id_dispatch),
 ]
