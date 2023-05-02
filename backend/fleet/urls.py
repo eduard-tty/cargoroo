558c5c7105b997aaaa4533ffcfc437a3ce740943
eduard <elohmann@xs4all.nl>
2023-05-02 13:54:01 +0200

    More tidy

--- backend/fleet/urls.py
+++ backend/fleet/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view as swagger_get_schema_view
 from . import views
 
-schema_view = swagger_get_schema_view(
+SchemaView = swagger_get_schema_view(
     openapi.Info(
         title='Cargoroo REST API',
         default_version='1.0.0',
@@ -17,5 +17,5 @@ urlpatterns = [
     path('fleet/<str:fid>', views.FleetItem.as_view()),
     path('fleet/<str:fid>/bike', views.BikesInFleet.as_view()),
     path('bike/<str:bid>', views.BikeItem.as_view()),
-    path('', schema_view.with_ui()),
+    path('', SchemaView.with_ui()),
 ]
